e1792f1fe4c37e2730e086bf94a1fbd16e1473ae
root <root@web0001.pexxi.eu>
2021-05-20 21:41:02 +0000

    update

--- ComGate/ComGateGateway/Model/Payment.php
+++ ComGate/ComGateGateway/Model/Payment.php
@@ -16,7 +16,7 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod {
   protected $_canFetchTransactionInfo = true;
 
   protected $_supportedCurrencyCodes = array(
-    'EUR'
+    'EUR', 'CZK', 'PLN', 'HUF', 'RON', 'HRK', 'USD', 'GPB', 'NOK', 'SEC'
   );
 
   public function __construct(\Magento\Framework\Model\Context $context, \Magento\Framework\Registry $registry, \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory, \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory, \Magento\Payment\Helper\Data $paymentData, \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, \Magento\Payment\Model\Method\Logger $logger, \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null, \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null, array $data = [], $directory = null) {
